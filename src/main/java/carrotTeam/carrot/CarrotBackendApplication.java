1c457008bd28da8a4563b870f2c4f56ced54cede
jung <mira20932@gmail.com>
2023-03-19 17:18:37 +0900

    초기 설정 세팅

---
+++ src/main/java/carrotTeam/carrot/CarrotBackendApplication.java
@@ -0,0 +1,13 @@
+package carrotTeam.carrot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrotBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrotBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/carrotTeam/carrot/CarrotBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package carrotTeam.carrot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrotBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
