f54b5f6b07ae713040d0828abe28aff519ad3888
jung <mira20932@gmail.com>
2023-03-20 00:32:45 +0900

    feat: BaseEntity 생성

---
+++ src/main/java/carrotTeam/carrot/global/common/BaseEntity.java
@@ -0,0 +1,31 @@
+package carrotTeam.carrot.global.common;
+
+import lombok.Getter;
+import org.hibernate.annotations.DynamicInsert;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(value = {AuditingEntityListener.class})
+@Getter
+@DynamicInsert
+
+public abstract class BaseEntity {
+  @CreatedDate
+  @Column(name = "created_at", updatable = false)
+  private LocalDateTime createdAt;
+
+  @LastModifiedDate
+  @Column(name = "updated_at")
+  private LocalDateTime updatedAt;
+
+  @Column(name = "is_active", columnDefinition = "tinyint(1) default 1", nullable = false)
+  private Boolean isActive = true;
+
+}
