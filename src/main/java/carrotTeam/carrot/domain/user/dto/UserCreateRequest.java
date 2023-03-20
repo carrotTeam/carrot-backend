d60fe952afa52d3ae76e22dee692f2948be8da5d
jung <mira20932@gmail.com>
2023-03-20 23:46:20 +0900

    feat: user 생성 api 구현

---
+++ src/main/java/carrotTeam/carrot/domain/user/dto/UserCreateRequest.java
@@ -0,0 +1,23 @@
+package carrotTeam.carrot.domain.user.dto;
+
+import lombok.*;
+
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+
+@Builder
+@Getter
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class UserCreateRequest {
+
+    @NotBlank(message = "이메일은 공백일 수 없습니다")
+    private String email;
+
+    @NotBlank(message = "비밀번호는 공백일 수 없습니다")
+    private String password;
+
+    @NotBlank(message = "자신의 위치를 입력해주세요")
+    private String location;
+
+}
---
+++ src/main/java/carrotTeam/carrot/domain/user/mapper/UserMapper.java
@@ -0,0 +1,27 @@
+package carrotTeam.carrot.domain.user.mapper;
+import carrotTeam.carrot.domain.user.domain.entity.User;
+import carrotTeam.carrot.domain.user.dto.UserCreateRequest;
+import carrotTeam.carrot.domain.user.dto.UserInfo;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper {
+
+    public User mapCreateUserToEntity(UserCreateRequest userCreateRequest){
+        return User.builder()
+                .email(userCreateRequest.getEmail())
+                .password(userCreateRequest.getEmail())
+                .location(userCreateRequest.getLocation())
+                .build();
+    }
+
+    public UserInfo mapUserEntityToUserInfo(User user) {
+        return UserInfo.builder()
+                .email(user.getEmail())
+                .password(user.getPassword())
+                .location(user.getLocation())
+                .createdAt(user.getCreatedAt())
+                .build();
+    }
+
+}
