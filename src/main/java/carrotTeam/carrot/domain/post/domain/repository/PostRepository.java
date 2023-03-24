2f391c3f8c00a030dc8c258bcd13fc318bfb1ef3
fnzl54 <rnjscksdud1@naver.com>
2023-03-25 04:23:51 +0900

    feat : Post Repository 추가

--- src/main/java/carrotTeam/carrot/domain/post/domain/repository/PostRepository.java
+++ src/main/java/carrotTeam/carrot/domain/post/domain/repository/PostRepository.java
@@ -1,5 +1,6 @@
-package carrotTeam.carrot.domain.post.domain;
+package carrotTeam.carrot.domain.post.domain.repository;
 
+import carrotTeam.carrot.domain.post.domain.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
