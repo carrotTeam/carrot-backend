53143d8a17ba84ee5b7a1ff1a16a8b8f9691fa1e
fnzl54 <rnjscksdud1@naver.com>
2023-03-29 23:05:35 +0900

    feat : post controller update 메소드 추가

--- src/main/java/carrotTeam/carrot/domain/post/controller/PostController.java
+++ src/main/java/carrotTeam/carrot/domain/post/controller/PostController.java
@@ -2,6 +2,7 @@ package carrotTeam.carrot.domain.post.controller;
 
 import carrotTeam.carrot.domain.post.dto.PostInfo;
 import carrotTeam.carrot.domain.post.dto.PostRequest;
+import carrotTeam.carrot.domain.post.dto.PostUpdateRequest;
 import carrotTeam.carrot.domain.post.service.PostService;
 
 import lombok.RequiredArgsConstructor;
@@ -22,5 +23,10 @@ public class PostController {
     public PostInfo uploadFile(@RequestPart(value = "images", required = false) MultipartFile multipartFile, @RequestPart(value = "request") PostRequest request) throws IOException {
         String picture_address = service.upload(multipartFile);
         return service.createPost(request, picture_address);
+    }
+
+    @PutMapping(value = "/update")
+    public PostInfo updatePost(@RequestBody PostUpdateRequest request) throws IOException {
+        return service.updatePost(request.getPost_id(), request.getTitle(), request.getContent());
     }
 }
