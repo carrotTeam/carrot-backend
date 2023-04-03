e07122ac15199745b948f1efa2ff98190452cb1b
fnzl54 <rnjscksdud1@naver.com>
2023-04-03 17:56:36 +0900

    feat : post repository 조회 함수 추가

--- src/main/java/carrotTeam/carrot/domain/post/domain/repository/PostRepository.java
+++ src/main/java/carrotTeam/carrot/domain/post/domain/repository/PostRepository.java
@@ -2,8 +2,19 @@ package carrotTeam.carrot.domain.post.domain.repository;
 
 import carrotTeam.carrot.domain.post.domain.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    List<Post> findAllByIsActive(Boolean isActive);
+
+    @Query(value = "SELECT p FROM Post p WHERE p.user.id = :id AND p.isActive = :isActive")
+    List<Post> findByIdAndIsActive(Long id, Boolean isActive);
+
+    @Query(value = "SELECT p FROM Post p WHERE (p.title LIKE %:word% OR p.content LIKE %:word%) AND p.isActive = :isActive")
+    List<Post> findByWordAndIsActive(String word, Boolean isActive);
 }
