3f4fe436209f10e5862800da58f14cc51fa0f24d
kwongwangjae <kgy30002@gmail.com>
2023-04-11 19:51:45 +0900

    feat: #16내용 재업로드

--- src/main/java/carrotTeam/carrot/domain/user/controller/UserController.java
+++ src/main/java/carrotTeam/carrot/domain/user/controller/UserController.java
@@ -2,12 +2,11 @@ package carrotTeam.carrot.domain.user.controller;
 
 import carrotTeam.carrot.domain.user.dto.UserCreateRequest;
 import carrotTeam.carrot.domain.user.dto.UserInfo;
+import carrotTeam.carrot.domain.user.dto.UserUpdateRequest;
 import carrotTeam.carrot.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -23,5 +22,24 @@ public class UserController {
         UserInfo userInfo = userService.createUser(request);
         return ResponseEntity.ok(userInfo);
     }
+
+    @GetMapping("/api/UserId/{id}")
+    public ResponseEntity getUser(@PathVariable("id") Long id){
+        UserInfo userInfo = userService.findUserId(id);
+        return ResponseEntity.ok(userInfo);
+    }
+
+    @PutMapping("/api/User/Change")
+    public ResponseEntity updateUser(@Valid @RequestBody UserUpdateRequest request){
+        UserInfo userInfo = userService.updateUser(request);
+        return ResponseEntity.ok(userInfo);
+    }
+
+    @DeleteMapping("/api/User/Delete/{id}")
+    public ResponseEntity deleteUser(@PathVariable("id") Long id){
+        UserInfo userInfo = userService.DeleteUser(id);
+        return  ResponseEntity.ok(userInfo);
+    }
+
 
 }
