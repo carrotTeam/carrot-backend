6ee80d8956e9ed15015b7fb646700d23f87c5cc4
jung <mira20932@gmail.com>
2023-05-02 13:47:54 +0900

    feat: 댓글(comment)entity 추가

--- src/main/java/carrotTeam/carrot/domain/comment/domain/entity/Comment.java
+++ src/main/java/carrotTeam/carrot/domain/comment/domain/entity/Comment.java
@@ -5,14 +5,32 @@ import carrotTeam.carrot.domain.user.domain.entity.User;
 import carrotTeam.carrot.global.common.BaseEntity;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "Comment")
 public class Comment extends BaseEntity {
 
+    @NotNull
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "comment_id", nullable = false)
+    @Column(name = "comment_id")
     private Long id;
+
+    @NotNull
+    @Column(name = "content", length = 100)
+    private String content;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
 
 }
