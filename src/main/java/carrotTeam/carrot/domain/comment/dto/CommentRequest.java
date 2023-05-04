04003e1af2f8b8b23e0cbb561660a45de46050e2
jung <mira20932@gmail.com>
2023-05-04 18:56:28 +0900

    feat: commenntRequest에 빌더와 user_id 추가

--- src/main/java/carrotTeam/carrot/domain/comment/dto/CommentRequest.java
+++ src/main/java/carrotTeam/carrot/domain/comment/dto/CommentRequest.java
@@ -1,13 +1,22 @@
 package carrotTeam.carrot.domain.comment.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.Getter;
+import lombok.*;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
 public class CommentRequest {
+
+    @NotNull
+    @Schema(description = "user 아이디")
+    private Long user_id;
 
     @NotNull
     @Schema(description = "post 아이디")
@@ -16,7 +25,6 @@ public class CommentRequest {
     @Schema(description = "comment 내용")
     @NotNull
     private String content;
-
 
 
 }
