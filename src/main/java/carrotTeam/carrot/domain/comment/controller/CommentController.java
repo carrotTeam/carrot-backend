48c5778eef9f76781433c2077b6ea0a1913fde1f
jung <mira20932@gmail.com>
2023-05-07 17:35:21 +0900

    refactor: 댓글 생성 시 resultcode 메세지 추가

--- src/main/java/carrotTeam/carrot/domain/comment/controller/CommentController.java
+++ src/main/java/carrotTeam/carrot/domain/comment/controller/CommentController.java
@@ -3,6 +3,8 @@ package carrotTeam.carrot.domain.comment.controller;
 import carrotTeam.carrot.domain.comment.domain.entity.Comment;
 import carrotTeam.carrot.domain.comment.dto.CommentInfo;
 import carrotTeam.carrot.domain.comment.dto.CommentRequest;
+import carrotTeam.carrot.global.result.ResultCode;
+import carrotTeam.carrot.global.result.ResultResponse;
 import org.springframework.web.bind.annotation.*;
 import carrotTeam.carrot.domain.comment.service.CommentService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -21,10 +23,10 @@ public class CommentController {
     private final CommentService commentService;
 
     @PostMapping("/api/comments")
-    public ResponseEntity createComment(@Valid @RequestBody CommentRequest commentRequest) {
+    public ResponseEntity<ResultResponse> createComment(@Valid @RequestBody CommentRequest commentRequest) {
 
         CommentInfo commentInfo = commentService.createComment(commentRequest);
-        return ResponseEntity.ok(commentInfo);
+        return ResponseEntity.ok(ResultResponse.of(ResultCode.CREATE_COMMENT_SUCCESS, commentInfo));
     }
 
 
