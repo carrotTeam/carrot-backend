6d337c1abae58725253596e92d351cef674bdcec
jung <mira20932@gmail.com>
2023-05-07 17:18:02 +0900

    chore: RuntimeException을 상속받아 사용자 예외처리 클래스 구현

---
+++ src/main/java/carrotTeam/carrot/global/error/exception/BusinessException.java
@@ -0,0 +1,14 @@
+package carrotTeam.carrot.global.error.exception;
+
+import carrotTeam.carrot.global.error.ErrorCode;
+import lombok.Getter;
+
+@Getter
+public class BusinessException extends RuntimeException{
+    private final ErrorCode errorCode;
+
+    public BusinessException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+}
