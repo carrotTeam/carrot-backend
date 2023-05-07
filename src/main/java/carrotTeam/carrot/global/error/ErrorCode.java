e82db4e868f5b9125b56af8a161a7750fd58e55c
jung <mira20932@gmail.com>
2023-05-07 17:09:42 +0900

    chore: error 메세지를 관리하는 Enum 클래스 구현

---
+++ src/main/java/carrotTeam/carrot/global/error/ErrorCode.java
@@ -0,0 +1,27 @@
+package carrotTeam.carrot.global.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/** {주체}_{이유} message 는 동사 명사형으로 마무리 */
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+
+    // Global
+    INTERNAL_SERVER_ERROR(500, "G001", "서버 오류"),
+    INPUT_INVALID_VALUE(400, "G002", "잘못된 입력"),
+
+
+//     User
+
+//     Post
+
+//     Comment
+    ;
+
+    private final int status;
+    private final String code;
+    private final String message;
+}
