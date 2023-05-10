31c9cbade260ced38ee4eb06090ffcc9c66b3930
jung <mira20932@gmail.com>
2023-05-10 17:13:15 +0900

    chore: 댓글 삭제 시 response 메세지 구현

--- src/main/java/carrotTeam/carrot/global/result/ResultCode.java
+++ src/main/java/carrotTeam/carrot/global/result/ResultCode.java
@@ -16,6 +16,7 @@ public enum ResultCode {
     // comment
     CREATE_COMMENT_SUCCESS("C001", "200", "댓글이 등록되었습니다!"),
     GET_ONE_POST_COMMENT_SUCCESS("C002", "200", "게시글의 댓글을 조회하였습니다!"),
+    DELETE_COMMENT_SUCCESS("C003","200","게시글의 댓글을 삭제하였습니다!"),
     ;
 
     private final String code;
