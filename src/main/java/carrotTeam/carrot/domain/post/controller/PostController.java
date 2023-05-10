1d7310b85a24d1a4e3d74f4caed91ef9735a01d5
fnzl54 <rnjscksdud1@naver.com>
2023-05-11 00:37:17 +0900

    refactor: post api 주소 수정

--- src/main/java/carrotTeam/carrot/domain/post/controller/PostController.java
+++ src/main/java/carrotTeam/carrot/domain/post/controller/PostController.java
@@ -13,41 +13,51 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.List;
 
+@RequestMapping("/api/posts")
 @RequiredArgsConstructor
 @RestController
 public class PostController {
 
     private final PostService service;
 
-
-    @PostMapping(value = "/upload", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE, MediaType.APPLICATION_JSON_VALUE})
+    @PostMapping(consumes = {MediaType.MULTIPART_FORM_DATA_VALUE, MediaType.APPLICATION_JSON_VALUE})
     public PostInfo uploadFile(@RequestPart(value = "images", required = false) MultipartFile multipartFile, @RequestPart(value = "request") PostRequest request) throws IOException {
         String picture_address = service.upload(multipartFile);
         return service.createPost(request, picture_address);
     }
 
-    @PutMapping(value = "/update")
-    public PostInfo updatePost(@RequestBody PostUpdateRequest request) throws IOException {
+    @PutMapping
+    public PostInfo updatePost (
+            @RequestBody PostUpdateRequest request
+    ) throws IOException {
         return service.updatePost(request.getPost_id(), request.getTitle(), request.getContent());
     }
 
-    @DeleteMapping(value = "/delete/{id}")
-    public PostInfo updatePost(@PathVariable Long id) throws IOException {
+    @DeleteMapping("/{id}")
+    public PostInfo updatePost (
+            @PathVariable Long id
+    ) throws IOException {
         return service.deletePost(id);
     }
 
-    @GetMapping(value = "/read")
-    public List<PostInfo> findTotalRestaurant () {
+    @GetMapping
+    public List<PostInfo> findTotalPost (
+
+    ) throws IOException {
         return service.findTotal();
     }
 
-    @GetMapping(value = "/read/{id}")
-    public List<PostInfo> findTotalRestaurant (@PathVariable Long id) {
+    @GetMapping("/{id}")
+    public List<PostInfo> findTotalRestaurant (
+            @PathVariable Long id
+    ) throws IOException {
         return service.findById(id);
     }
 
-    @GetMapping(value = "/read/word")
-    public List<PostInfo> findTotalRestaurant (@RequestParam String word) {
+    @GetMapping("/word/{word}")
+    public List<PostInfo> findTotalRestaurant (
+            @RequestParam String word
+    ) throws IOException {
         return service.findByWord(word);
     }
 
