519e0a2d66f85f6f7c81b5d2a575e0acbaad591a
jung <mira20932@gmail.com>
2023-05-12 23:41:39 +0900

    feat: 연관관계의 주인이 parentcomment인 조회용 childrencomment 추가

--- src/main/java/carrotTeam/carrot/domain/comment/domain/entity/Comment.java
+++ src/main/java/carrotTeam/carrot/domain/comment/domain/entity/Comment.java
@@ -3,6 +3,8 @@ package carrotTeam.carrot.domain.comment.domain.entity;
 import carrotTeam.carrot.domain.post.domain.entity.Post;
 import carrotTeam.carrot.domain.user.domain.entity.User;
 import carrotTeam.carrot.global.common.BaseEntity;
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -11,6 +13,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -45,6 +48,8 @@ public class Comment extends BaseEntity {
   @JoinColumn(name = "parent_id")
   private Comment parentComment;
 
+  @OneToMany(mappedBy = "parentComment", orphanRemoval = true)
+  private List<Comment> childrenComment = new ArrayList<>();
 
   @Builder
   private Comment(String content, Post post, User user) {
