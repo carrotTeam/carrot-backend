7a9a6481b6da5e970f73756e4fb8202a9934cd85
jung <mira20932@gmail.com>
2023-05-13 21:14:51 +0900

    refactor: create_At과 update_At을 HH:mm:ss로 포맷팅

--- src/main/java/carrotTeam/carrot/domain/comment/dto/CommentInfo.java
+++ src/main/java/carrotTeam/carrot/domain/comment/dto/CommentInfo.java
@@ -1,20 +1,20 @@
 package carrotTeam.carrot.domain.comment.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
 
 
 @Getter
 @NoArgsConstructor
 public class CommentInfo {
 
-  @DateTimeFormat
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
   private LocalDateTime createAt;
 
-  @DateTimeFormat
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
   private LocalDateTime updateAt;
 
   private String content;
