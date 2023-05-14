8d08cb67af33a49175f48630f50f4bf838873c2f
fnzl54 <rnjscksdud1@naver.com>
2023-05-15 01:54:03 +0900

    feat: 작성자 이외에 게시글 수정과 삭제시 오류 코드

---
+++ src/main/java/carrotTeam/carrot/domain/post/exception/NotUpdatePost.java
@@ -0,0 +1,11 @@
+package carrotTeam.carrot.domain.post.exception;
+
+import carrotTeam.carrot.global.error.ErrorCode;
+import carrotTeam.carrot.global.error.exception.BusinessException;
+
+public class NotUpdatePost extends BusinessException {
+  public NotUpdatePost() {
+    super(ErrorCode.POST_NOT_UPDATE);
+  }
+
+}
