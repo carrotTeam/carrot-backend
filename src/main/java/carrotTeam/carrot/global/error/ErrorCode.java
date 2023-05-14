4d51457cfff10c1141784ce5f7f711b74a828b4b
fnzl54 <rnjscksdud1@naver.com>
2023-05-14 16:44:22 +0900

    feat: 게시들 업로드 문제시 에러코드 추가

--- src/main/java/carrotTeam/carrot/global/error/ErrorCode.java
+++ src/main/java/carrotTeam/carrot/global/error/ErrorCode.java
@@ -17,6 +17,7 @@ public enum ErrorCode {
 
     // Post
     POST_NOT_FOUND(400, "P001","게시글이 존재하지 않습니다!"),
+    POST_NOT_UPLOAD(400, "P002","게시글이 업로드에 실패하였습니다!"),
 
     // Comment
     COMMENT_NOT_FOUND(400,"C001","댓글이 존재하지 않습니다!")
