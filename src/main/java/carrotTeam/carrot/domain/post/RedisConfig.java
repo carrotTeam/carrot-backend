3ba69725e3cf9b8743533b7df4f7ec9acdbe715d
fnzl54 <rnjscksdud1@naver.com>
2023-05-16 21:42:43 +0900

    feat: redis 사용을 위한 config 파일 추가

---
+++ src/main/java/carrotTeam/carrot/domain/post/RedisConfig.java
@@ -0,0 +1,49 @@
+package carrotTeam.carrot.domain.post;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import java.time.Duration;
+
+@Configuration
+public class RedisConfig extends CachingConfigurerSupport {
+
+  @Value("${spring.redis.host}")
+  private String host;
+
+  @Value("${spring.redis.port}")
+  private int port;
+
+  @Bean
+  public LettuceConnectionFactory redisConnectionFactory() {
+    RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(
+        host, port);
+    return new LettuceConnectionFactory(host, port);
+  }
+
+
+  @Bean
+  public RedisTemplate<?, ?> redisTemplate() {
+    RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
+    redisTemplate.setConnectionFactory(redisConnectionFactory());
+    return redisTemplate;
+  }
+
+  @Bean
+  public CacheManager cacheManager() {
+    RedisCacheConfiguration cacheConfig = RedisCacheConfiguration.defaultCacheConfig()
+        .entryTtl(Duration.ofSeconds(60)); // 60초 만료시간으로 설정
+    return RedisCacheManager.builder(redisConnectionFactory())
+        .cacheDefaults(cacheConfig)
+        .build();
+  }
+}
+
